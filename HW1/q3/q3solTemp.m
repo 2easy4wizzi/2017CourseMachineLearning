1ab39c94b2dcbd3453dcc43063dc47ce695b0d7b
2easy4wizzi <giladeini@gmail.com>
2017-12-27 17:00:58 +0200

    q3 fix + temp

--- HW1/q3/q3solTemp.m
+++ HW1/q3/q3solTemp.m
@@ -1,7 +1,7 @@
 %authors: 
 % Matan Finch, id 300895315
 % Gilad Eini , id 034744920
-function q3sol()
+function q3solTemp()
     clear;
     close all;
     clc;
@@ -16,38 +16,33 @@ function q3sol()
     testB = test_dataB;
     clear train_dataA train_dataB valid_dataA valid_dataB test_dataA test_dataB;
 
-    %use valid data to get the gausian window function
-    covValidA = cov(validA); 
-    covValidB = cov(validB);
-    
-%     suc = pnn(trainA, trainB, validA, validB, covValidA, covValidB );
-%     disp(['succes rate(in fraction) for valid data: ' num2str(suc)]);
 
-    suc = pnn(trainA, trainB, testA, testB, covValidA , covValidB);
+    % windowSize = 2;
+    % suc = pnn(trainA, trainB, validA, validB);
+    % disp(['succes rate(in fraction) for valid data: ' num2str(suc)]);
+
+
+    suc = pnn(trainA, trainB, testA, testB);
     disp(['succes rate(in fraction) for test data: ' num2str(suc)]);
     disp('done');
 end
 
-function suc = pnn(trainA, trainB, testedDataA, testedDataB, covValidA, covValidB )  
-
+function suc = pnn(trainA, trainB, testedDataA, testedDataB)  
     [trainAsize, ~] = size(trainA);
     [trainBsize, ~] = size(trainB);
     [testedAsize, ~] = size(testedDataA);
     [testedBsize, ~] = size(testedDataB);
     totalCorrect = 0; %total clasified correct
-    
-%     covMatA = cov(trainA);%cov matrix - used in the multiDimGaus 
-%     covMatB = cov(trainB);
-
+    sig = 2;
     %calculate algoritm for class A. count only if Ya > Yb
-    covMatA = covValidA;
-    covMatB = covValidB;
+    covMatA = cov(trainA);%cov matrix - used in the multiDimGaus 
     for i=1 : testedAsize %go over all tested samples (could be valid data or test data)
         valueFromA = 0;
         %calculate Ya(tested sample, trainDataA). for each train sample calculate the gausian with the
         %current tested sample and sum it up
         for j=1 : trainAsize
-            valueFromA = valueFromA + multiDimGaus(trainA(j,:), testedDataA(i,:), covMatA);
+%             valueFromA = valueFromA + multiDimGaus(trainA(j,:), testedDataA(i,:), covMatA);
+            valueFromA = valueFromA + singleDimGaus(trainA(j,:), testedDataA(i,:), sig);
         end
         %normalize with the number of train samples.
         valueFromA = valueFromA/trainAsize;
@@ -55,30 +50,30 @@ function suc = pnn(trainA, trainB, testedDataA, testedDataB, covValidA, covValid
         %do to this current tested sample the same with trainDataB - Yb(tested sample, trainDataB)
         valueFromB = 0;
         for j=1 : trainBsize
-            valueFromB = valueFromB + multiDimGaus(trainB(j,:), testedDataA(i,:), covMatB);
+%             valueFromB = valueFromB + multiDimGaus(trainB(j,:), testedDataA(i,:), covMatB);
+            valueFromB = valueFromB + singleDimGaus(trainB(j,:), testedDataA(i,:), sig);
         end
         valueFromB = valueFromB/trainBsize;
-        
         if(valueFromA > valueFromB)% if Ya > Yb -> we clasified correctly
             totalCorrect = totalCorrect + 1;
         end
     end
     
     %the same as explaind up just for testedDataB
-    
+    covMatB = cov(trainB);
     for i=1 : testedBsize
         valueFromA = 0;
         for j=1 : trainAsize
-            valueFromA = valueFromA + multiDimGaus(trainA(j,:), testedDataB(i,:), covMatA);
+%             valueFromA = valueFromA + multiDimGaus(trainA(j,:), testedDataB(i,:), covMatA);
+            valueFromA = valueFromA + singleDimGaus(trainA(j,:), testedDataB(i,:), sig);
         end
         valueFromA = valueFromA/trainAsize;
-        
         valueFromB = 0;
         for j=1 : trainBsize
-            valueFromB = valueFromB + multiDimGaus(trainB(j,:), testedDataB(i,:), covMatB);
+%             valueFromB = valueFromB + multiDimGaus(trainB(j,:), testedDataB(i,:), covMatB);
+            valueFromB = valueFromB + singleDimGaus(trainB(j,:), testedDataB(i,:), sig);
         end
         valueFromB = valueFromB/trainBsize;
-        
         if(valueFromA < valueFromB)%if Ya < Yb -> we clasified correctly
             totalCorrect = totalCorrect + 1;
         end
@@ -89,6 +84,10 @@ end
 
 function scalar = multiDimGaus(sampleVecValue, testedVecValue, covMat)
      scalar = (1/(sqrt(det(2*pi*covMat)))) * exp(-1/2*(sampleVecValue-testedVecValue)*inv(covMat)*(sampleVecValue-testedVecValue)');
+end
+
+function scalar = singleDimGaus(sampleVecValue, testedVecValue, sig)
+    scalar = (1/sqrt(2*pi)*sig)*exp(-1*power(norm(sampleVecValue-testedVecValue),2)/(2*power(sig,2)));
 end
 
 
