b339ee981d8f330163d8971091112833a028935b
2easy4wizzi <giladeini@gmail.com>
2018-01-23 17:47:06 +0200

    q2

--- HW2/hw/q2/runLinearRegression.m
+++ HW2/hw/q2/runLinearRegression.m
@@ -4,27 +4,25 @@ function runLinearRegression
     clc;
     load('regdata.mat', 'R');
     
-    %clause a
+    %%clause a
     [w, MSE] = trainOnDataAndCalcWandMSE(R);
     disp(['The MSE:'  num2str(MSE) '. The optimal W: [' num2str(w(:).') ']']) ;
-    %clause b - split and: calc W and MSE on trainData,calc MSE on testData
+    %%clause b - split and: calc W and MSE on trainData,calc MSE on testData
     [Rrows, ~] = size(R);
     splits = [10 50 100 200];
     for i=splits
         disp(['case split is ' num2str(i) ' and ' num2str(Rrows-i)]);
        [trainData, testData] = permAndSplit(R,i); 
        [w, MSE] = trainOnDataAndCalcWandMSE(trainData);
-       disp(['  On |trainData| =' num2str(i)]);
-       disp(['  The MSE:'  num2str(MSE) '. The optimal W: [' num2str(w(:).') ']']) ;
+       disp(['       On |trainData| =' num2str(i)]);
+       disp(['       The MSE:'  num2str(MSE) '. The optimal W: [' num2str(w(:).') ']']) ;
        MSE = testOnDataAndCalcMSE(testData,w);
-       disp(['  On |testData| =' num2str(Rrows-i)]);
-       disp(['  The MSE:'  num2str(MSE)]) ;
+       disp(['       On |testData| =' num2str(Rrows-i)]);
+       disp(['       The MSE:'  num2str(MSE)]) ;
     end
-    disp('bi');
 end
 
 function [w,MSE] = trainOnDataAndCalcWandMSE(R)
-    
     [Rrows, Rcols] = size(R);
     X = R(:, 2:Rcols);%chop train samples
     y = R(:,1);%chop first column
